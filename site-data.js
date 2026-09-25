c6bbab77948e7403ea7ecd69bd9fdcc77d78092d
fradalex <fradalessandro98@gmail.com>
2026-09-25 14:49:54 +0200

    Prepare Mailing list section and Brevo form integration

--- site-data.js
+++ site-data.js
@@ -2,6 +2,7 @@
 window.PDS_DATA = {
   email: "paperdevelopmentseries@gmail.com", // Public contact address and contact form destination
   proposalUrl: "", // Optional Google Form or other submission link; use a full https:// URL
+  mailingListFormUrl: "", // Paste the HTTPS iframe src from Brevo > Marketing > Forms > Share
   organisers: [], // Names, e.g. ["Alex Smith", "Sam Lee"]
   sessions: [
     {
